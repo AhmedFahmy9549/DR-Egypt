80a9349255a8cd17db41767907dfc8961847f3bd
ZeraW <hema_eie552@yahoo.com>
2018-05-05 17:32:20 +0200

    added search bar

--- app/src/main/java/com/example/gmsproduction/dregypt/ui/activities/ProductsActivity.java
+++ app/src/main/java/com/example/gmsproduction/dregypt/ui/activities/ProductsActivity.java
@@ -1,11 +1,17 @@
 package com.example.gmsproduction.dregypt.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -18,6 +24,7 @@ import com.example.gmsproduction.dregypt.Data.remoteDataSource.NetworkRequests.P
 import com.example.gmsproduction.dregypt.R;
 import com.example.gmsproduction.dregypt.ui.adapters.ProductAdsAdapter;
 import com.example.gmsproduction.dregypt.utils.ProductsModel;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,6 +39,7 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
     private ArrayList<ProductsModel> modelArrayList;
     String id, title, description, price, image, status,address,created_at,phone_1,phone_2;
     SliderLayout mDemoSlider;
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +47,46 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
         setContentView(R.layout.recycler_products);
         GetProductAdsRequest getProductAdsRequest = new GetProductAdsRequest(this, this, this);
         getProductAdsRequest.start();
-
+        
+        //slider
         mDemoSlider = (SliderLayout) findViewById(R.id.ProductsSilder);
-
         //arrayLIST
         modelArrayList = new ArrayList<>();
-
         //recycler View horizon orientation
         mRecyclerView = findViewById(R.id.Recycler_Product);
         LinearLayoutManager LayoutManagaer = new GridLayoutManager(ProductsActivity.this, 3);
         mRecyclerView.setLayoutManager(LayoutManagaer);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarxx);
+        setSupportActionBar(toolbar);
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setVoiceSearch(false);
+        searchView.setEllipsize(true);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
     }
 
     @Override
@@ -62,7 +99,6 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
 
         Log.e("tagyyy",response);
         try {
-
             JSONObject object = new JSONObject(response);
             JSONArray dataArray = object.getJSONArray("data");
             for (int a = 0; a < dataArray.length(); a++) {
@@ -70,7 +106,7 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
                 id = dataObject.getString("id");
                 title = dataObject.getString("title");
                 price = dataObject.getString("price");
-                image = dataObject.getString("img");
+                image = "https://dregy.frb.io"+dataObject.getString("img");
                 status = dataObject.getString("status");
                 address = dataObject.getString("address");
                 created_at = dataObject.getString("created_at");
@@ -79,7 +115,6 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
                 phone_1 = (String) phoneArray.get(0);
                 phone_2 = (String) phoneArray.get(1);
                 modelArrayList.add(new ProductsModel(id,title,description,price,status,image,address,created_at,phone_1,phone_2));
-
             }
             loadIMG(modelArrayList);
             mAdapter = new ProductAdsAdapter(ProductsActivity.this, modelArrayList);
@@ -143,6 +178,44 @@ public class ProductsActivity extends AppCompatActivity implements Response.List
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    //menu option
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+
+    //on back press
+    @Override
+    public void onBackPressed() {
+        if (searchView.isSearchOpen()) {
+            searchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //still don't know how it's work
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
+
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
