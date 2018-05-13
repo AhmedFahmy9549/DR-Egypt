364e2912d499dda18d9acd7bce49fee0d4160ce5
Fahmy <ahmedmf9549@gmail.com>
2018-05-13 13:14:33 +0200

    new Project Added

--- app/src/main/java/com/example/gmsproduction/dregypt/ui/activities/HospitalsActivity.java
+++ app/src/main/java/com/example/gmsproduction/dregypt/ui/activities/HospitalsActivity.java
@@ -1,65 +1,133 @@
 package com.example.gmsproduction.dregypt.ui.activities;
 
-import android.support.v4.content.ContextCompat;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.RatingBar;
-import android.widget.Toast;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ToggleButton;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.gmsproduction.dregypt.Data.remoteDataSource.NetworkRequests.HospitalsRequests.SearchHospitalsRequest;
+import com.example.gmsproduction.dregypt.Models.HospitalModel;
 import com.example.gmsproduction.dregypt.R;
-import com.example.gmsproduction.dregypt.ui.fragments.HospitalsFragment;
+import com.example.gmsproduction.dregypt.ui.fragments.FragmentsFilters.AdapterHospitalRecylcer;
+import com.example.gmsproduction.dregypt.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HospitalsActivity extends AppCompatActivity {
-    float x ;
-    ToggleButton toggleButton;
+
+    RecyclerView recyclerView;
+    View view;
+    String TAG = "HospitalsActivity";
+    HashMap<String, String> parms = new HashMap<>();
+    ArrayList<HospitalModel> arrayList = new ArrayList<>();
+    private AdapterHospitalRecylcer adapterx;
+    private Button btnFilter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospitals);
         setTitle("Hospitals");
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.container_hos,new HospitalsFragment(),"HospitalsFragment")
-                .commit();
-
-
-/*
-
-        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        ratingBar.setRating(x);
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-
-               */
-/* Toast.makeText(HospitalsActivity.this,
-                        "Rating changed, current rating " + ratingBar.getRating(),
-                        Toast.LENGTH_SHORT).show();*//*
+        recyclerView = findViewById(R.id.hospital_recycler);
 
 
 
-            }
-        });
+        SharedPreferences prefs = getSharedPreferences("Location", MODE_PRIVATE);
+        int city_id = prefs.getInt("city_id", 0); //0 is the default value.
+        int region_id = prefs.getInt("region_id", 0);
+        int special_id = prefs.getInt("specialId", 0);
 
-        toggleButton = (ToggleButton) findViewById(R.id.myToggleButton);
-        toggleButton.setChecked(false);
-        toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_favorite_black_24dp));
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_favorite_black_24dp_fill));
-                else
-                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_favorite_black_24dp));
 
-                //getSupportFragmentManager().beginTransaction().add(new SpecializationFragments(),"spec").commit();
-            }
-        });
-*/
+
+        Log.e(TAG, "Region=" + region_id + "  City=" + city_id + "    SpecialName=" + special_id);
+        getHospital(city_id,region_id,special_id);
 
     }
+
+
+    public void getHospital(int city_id,int region_id,int special_id ) {
+        parms.put("region", String.valueOf(region_id));
+        parms.put("city", String.valueOf(city_id));
+        parms.put("speciality", String.valueOf(special_id));
+        final SearchHospitalsRequest searchHospitalsRequest = new SearchHospitalsRequest(this, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                HospitalResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+
+            }
+        });
+        searchHospitalsRequest.setBody(parms);
+        searchHospitalsRequest.start();
+    }
+
+    public void HospitalResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                String name_hos = object.getString("en_name");
+                int id_hos = object.getInt("id");
+                String address_hos = object.getString("en_address");
+                String note_hos = object.getString("en_note");
+                String website_hos = object.getString("website");
+                String email_hos = object.getString("email");
+                String img_hos = Constants.ImgUrl + object.getString("img");
+                String createdAt_hos = object.getString("created_at");
+
+
+                JSONObject fav_object = object.getJSONObject("favorites");
+                int fav_hos = fav_object.getInt("count");
+
+                JSONObject rate = object.getJSONObject("rate");
+                int count_hos = rate.getInt("count");
+                float rating_hos = (float) rate.getDouble("rating");
+
+                JSONArray phone = object.getJSONArray("phone");
+                String phone_hos = phone.getString(0);
+                String phone2_hos = phone.getString(1);
+
+                Log.e(TAG + "Response=", "" + rating_hos);
+
+
+                HospitalModel model = new HospitalModel(id_hos, name_hos, address_hos, note_hos, website_hos, email_hos, img_hos, phone_hos, phone2_hos, count_hos, rating_hos, fav_hos, createdAt_hos);
+
+
+                arrayList.add(model);
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        adapterx = new AdapterHospitalRecylcer(this, arrayList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapterx);
+    }
+
+
 }
